f3d074fd33f9d25e5160f9e4f3a87455c058e2df
Ivan Tarskich <ivan.tarskich@edu.fh-joanneum.at>
2023-05-05 00:38:29 +0200

    init tauri cpu project

---
+++ tauri-cpu/src/main.js
@@ -0,0 +1,21 @@
+const { invoke } = window.__TAURI__.tauri;
+
+let greetInputEl;
+let greetMsgEl;
+
+async function greet() {
+  // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+  greetMsgEl.textContent = await invoke("greet", { name: greetInputEl.value });
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+  greetInputEl = document.querySelector("#greet-input");
+  greetMsgEl = document.querySelector("#greet-msg");
+  document
+    .querySelector("#greet-button")
+    .addEventListener("click", () => greet());
+  document.querySelector("#greet-button");
+  addEventListener("keypress", (event) => {
+    event.key === "Enter" ? (event.preventDefault(), greet()) : null;
+  });
+});
