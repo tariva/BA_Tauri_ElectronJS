bcabfb53a7dc17187b465a2a33ca93e4b818efd9
Ivan Tarskich <ivan.tarskich@edu.fh-joanneum.at>
2023-05-07 20:45:22 +0200

    added Ipc benchmark to electron

--- ipc/src/preload.js
+++ ipc/src/preload.js
@@ -1,2 +1,12 @@
 // See the Electron documentation for details on how to use preload scripts:
 // https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
+const { contextBridge, ipcRenderer } = require("electron");
+
+const SEND_CHANNEL = "asynchronous-message";
+const RECEIVE_CHANNEL = "asynchronous-reply";
+
+contextBridge.exposeInMainWorld("comms", {
+  sendMessage: (message) => ipcRenderer.send(SEND_CHANNEL, message),
+  onMessage: (handler) => ipcRenderer.on(RECEIVE_CHANNEL, handler),
+  clear: () => ipcRenderer.removeAllListeners(RECEIVE_CHANNEL),
+});
---
+++ ipc/src/render.js
@@ -0,0 +1,87 @@
+const startBurst = () => {
+  const totalMessages = parseInt(document.getElementById('messages').value);
+  let received = 0;
+  const startTime = Date.now();
+  const messages = [];
+
+  const onMessage = (event, arg) => {
+    const end = Date.now();
+    const message = arg;
+
+    message.duration = end - message.start;
+    messages.push(message);
+
+    received++;
+
+    if (received === totalMessages) {
+      const totalTime = end - startTime;
+      let durations = 0;
+
+      comms.clear();
+
+      for (const message of messages) {
+        durations += message.duration;
+      }
+      const average = durations / messages.length;
+      document.getElementById('results').innerText = `Total: ${totalTime.toFixed(2)}ms - avg message roundtrip: ${average.toFixed(2)}ms`;
+    }
+  }
+
+  comms.onMessage(onMessage);
+
+  for (let id = 1; id <= totalMessages; id++) {
+    const message = { id, start: Date.now(), duration: 0 };
+
+    comms.sendMessage(message);
+  }
+};
+
+document.getElementById('start').addEventListener('click', startBurst);
+
+const startSequential = () => {
+  const totalMessages = parseInt(document.getElementById('messages').value);
+  let received = 0;
+  const messages = [];
+  const startTime = performance.now();
+
+  const sendMessage = (id) => {
+    const message = { id, start: performance.now(), duration: 0 };
+
+    comms.sendMessage(message);
+  };
+
+  const onMessage = (event, arg) => {
+    const end = performance.now();
+    const message = arg;
+
+    if (!message) {
+      return;
+    }
+
+    message.duration = end - message.start;
+    messages.push(message);
+
+    received++;
+
+    if (received === totalMessages) {
+      const totalTime = end - startTime;
+      let durations = 0;
+
+      comms.clear();
+
+      for (const message of messages) {
+        durations += message.duration;
+      }
+      const average = durations / messages.length;
+      document.getElementById('results-sequential').innerText = `Total: ${totalTime.toFixed(2)}ms - avg message roundtrip: ${average.toFixed(2)}ms`;
+    } else {
+      sendMessage(received + 1);
+    }
+  };
+
+  comms.onMessage(onMessage);
+
+  sendMessage(received + 1);
+};
+
+document.getElementById('start-sequential').addEventListener('click', startSequential);
