82c20a3eaceff3742ff9f8f97e52dec80e59616b
Ivan Tarskich <ivan.tarskich@edu.fh-joanneum.at>
2023-05-08 12:16:36 +0200

    added tauri benchmark for ipc

--- tauri-ipc/src-tauri/src/main.rs
+++ tauri-ipc/src-tauri/src/main.rs
@@ -2,14 +2,29 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+use serde_json::from_str;
+use tauri::Manager;
+#[derive(Clone, serde::Serialize, serde::Deserialize)]
+pub struct Message {
+  id: usize,
+  start: f64,
+  duration: f64,
 }
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+       .setup(|app| {
+        let handle = app.handle();
+            // listen to the `asynchronous-message` (emitted on any window)
+            app.listen_global("asynchronous-message", move |event| {
+                let payload: &str = event.payload().unwrap();
+                let message: Message = from_str(payload).expect("failed to deserialize payload");
+                // emit the `asynchronous-reply` event to all webview windows on the frontend
+                handle.emit_all("asynchronous-reply", &message).expect("failed to emit event");
+            });
+
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- tauri-ipc/src/main.js
+++ tauri-ipc/src/main.js
@@ -1,17 +1,107 @@
-const { invoke } = window.__TAURI__.tauri;
+const SEND_CHANNEL = 'asynchronous-message';
+const RECEIVE_CHANNEL = 'asynchronous-reply';
+const { emit, listen } = window.__TAURI__.event;
+window.comms = {
+  sendMessage: (message) => {
+    emit(SEND_CHANNEL, message);
+  },
+  onMessage: (handler) => {
+    listen(RECEIVE_CHANNEL, (event) => {
+      handler(event.payload);
+    });
+  },
+  clear: () => {
+    // There is no clear equivalent in the new API.
+    // You can manage the unlisten functions returned by listen() if you want to remove event listeners.
+  },
+};
 
-let greetInputEl;
-let greetMsgEl;
+const startBurst = () => {
+  const totalMessages = parseInt(document.getElementById('messages').value);
+  let received = 0;
+  const startTime = Date.now();
+  const messages = [];
 
-async function greet() {
-  // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-  greetMsgEl.textContent = await invoke("greet", { name: greetInputEl.value });
-}
+  const onMessage = ( arg) => {
+    console.log("onMessage")
+    const end = Date.now();
+    const message = arg;
 
-window.addEventListener("DOMContentLoaded", () => {
-  greetInputEl = document.querySelector("#greet-input");
-  greetMsgEl = document.querySelector("#greet-msg");
-  document
-    .querySelector("#greet-button")
-    .addEventListener("click", () => greet());
-});
+    message.duration = end - message.start;
+    messages.push(message);
+
+    received++;
+    // log status
+    console.log(received+"/"+totalMessages)
+    if (received === totalMessages) {
+      const totalTime = end - startTime;
+      let durations = 0;
+
+      // comms.clear();
+
+      for (const message of messages) {
+        durations += message.duration;
+      }
+      const average = durations / messages.length;
+      document.getElementById('results').innerText = `Total: ${totalTime.toFixed(2)}ms - avg message roundtrip: ${average.toFixed(2)}ms`;
+    }
+  }
+
+  comms.onMessage(onMessage);
+
+  for (let id = 1; id <= totalMessages; id++) {
+    const message = { id, start: Date.now(), duration: 0 };
+
+    comms.sendMessage(message);
+  }
+};
+
+document.getElementById('start').addEventListener('click', startBurst);
+
+const startSequential = () => {
+  const totalMessages = parseInt(document.getElementById('messages').value);
+  let received = 0;
+  const messages = [];
+  const startTime = performance.now();
+
+  const sendMessage = (id) => {
+    const message = { id, start: performance.now(), duration: 0 };
+
+    comms.sendMessage(message);
+  };
+
+  const onMessage = ( arg) => {
+    const end = performance.now();
+    const message = arg;
+
+    if (!message) {
+      return;
+    }
+
+    message.duration = end - message.start;
+    messages.push(message);
+
+    received++;
+
+    if (received === totalMessages) {
+      const totalTime = end - startTime;
+      let durations = 0;
+
+      comms.clear();
+
+      for (const message of messages) {
+        durations += message.duration;
+      }
+      const average = durations / messages.length;
+      document.getElementById('results-sequential').innerText = `Total: ${totalTime.toFixed(2)}ms - avg message roundtrip: ${average.toFixed(2)}ms`;
+    } else {
+      sendMessage(received + 1);
+    }
+  };
+
+  comms.onMessage(onMessage);
+
+  sendMessage(received + 1);
+};
+
+document.getElementById('start-sequential').addEventListener('click', startSequential);
