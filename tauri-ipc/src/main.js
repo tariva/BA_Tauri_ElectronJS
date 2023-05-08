4368318d41e8d7ecee64edf595a85cac54310c7c
Ivan Tarskich <ivan.tarskich@edu.fh-joanneum.at>
2023-05-08 14:26:34 +0200

    tune tauri ipc bit

--- tauri-ipc/src/main.js
+++ tauri-ipc/src/main.js
@@ -1,107 +1,73 @@
 const SEND_CHANNEL = 'asynchronous-message';
 const RECEIVE_CHANNEL = 'asynchronous-reply';
 const { emit, listen } = window.__TAURI__.event;
+
 window.comms = {
-  sendMessage: (message) => {
-    emit(SEND_CHANNEL, message);
-  },
-  onMessage: (handler) => {
-    listen(RECEIVE_CHANNEL, (event) => {
-      handler(event.payload);
-    });
-  },
-  clear: () => {
-    // There is no clear equivalent in the new API.
-    // You can manage the unlisten functions returned by listen() if you want to remove event listeners.
-  },
+  sendMessage: (message) => emit(SEND_CHANNEL, message),
+  onMessage: (handler) => listen(RECEIVE_CHANNEL, (event) => handler(event.payload)),
+  clear: () => {} // No clear equivalent in the new API
+};
+
+const calculateResults = (messages, totalTime, resultElementId) => {
+  const durations = messages.reduce((sum, message) => sum + message.duration, 0);
+  const average = durations / messages.length;
+  document.getElementById(resultElementId).innerText = `Total: ${totalTime.toFixed(2)}ms - avg message roundtrip: ${average.toFixed(2)}ms`;
 };
 
 const startBurst = () => {
   const totalMessages = parseInt(document.getElementById('messages').value);
-  let received = 0;
-  const startTime = Date.now();
   const messages = [];
+  const startTime = Date.now();
+  let received = 0;
 
-  const onMessage = ( arg) => {
-    console.log("onMessage")
+  const onMessage = (message) => {
     const end = Date.now();
-    const message = arg;
-
     message.duration = end - message.start;
     messages.push(message);
-
     received++;
-    // log status
-    console.log(received+"/"+totalMessages)
+  
     if (received === totalMessages) {
-      const totalTime = end - startTime;
-      let durations = 0;
-
-      // comms.clear();
-
-      for (const message of messages) {
-        durations += message.duration;
-      }
-      const average = durations / messages.length;
-      document.getElementById('results').innerText = `Total: ${totalTime.toFixed(2)}ms - avg message roundtrip: ${average.toFixed(2)}ms`;
+      calculateResults(messages, end - startTime, 'results');
     }
-  }
+  };
+  
 
   comms.onMessage(onMessage);
 
   for (let id = 1; id <= totalMessages; id++) {
-    const message = { id, start: Date.now(), duration: 0 };
-
-    comms.sendMessage(message);
+    comms.sendMessage({ id, start: Date.now(), duration: 0 });
   }
 };
 
-document.getElementById('start').addEventListener('click', startBurst);
-
 const startSequential = () => {
   const totalMessages = parseInt(document.getElementById('messages').value);
-  let received = 0;
   const messages = [];
   const startTime = Date.now();
+  let received = 0;
 
   const sendMessage = (id) => {
-    const message = { id, start: Date.now(), duration: 0 };
-
-    comms.sendMessage(message);
+    comms.sendMessage({ id, start: Date.now(), duration: 0 });
   };
 
-  const onMessage = ( arg) => {
+  const onMessage = (message) => {
+    if (!message) return;
+  
     const end = Date.now();
-    const message = arg;
-
-    if (!message) {
-      return;
-    }
-
     message.duration = end - message.start;
     messages.push(message);
-
     received++;
-
+  
     if (received === totalMessages) {
-      const totalTime = end - startTime;
-      let durations = 0;
-
       comms.clear();
-
-      for (const message of messages) {
-        durations += message.duration;
-      }
-      const average = durations / messages.length;
-      document.getElementById('results-sequential').innerText = `Total: ${totalTime.toFixed(2)}ms - avg message roundtrip: ${average.toFixed(2)}ms`;
+      calculateResults(messages, end - startTime, 'results-sequential');
     } else {
       sendMessage(received + 1);
     }
   };
-
+  
   comms.onMessage(onMessage);
-
   sendMessage(received + 1);
 };
 
+document.getElementById('start').addEventListener('click', startBurst);
 document.getElementById('start-sequential').addEventListener('click', startSequential);
